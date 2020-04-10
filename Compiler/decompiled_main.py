55e708802984173b48d9da898d273d7e4d7a0be8
Igor <igor.musinov@phystech.edu>
2020-04-10 17:12:49 +0300

    python compiler

---
+++ Compiler/decompiled_main.py
@@ -0,0 +1,15 @@
+# uncompyle6 version 3.6.5
+# Python bytecode 3.7 (3394)
+# Decompiled from: Python 3.7.0 (default, Jun 28 2018, 13:15:42) 
+# [GCC 7.2.0]
+# Embedded file name: main.py
+# Size of source mod 2**32: 75 bytes
+
+
+def main():
+    print('Hello World')
+
+
+if __name__ == '__main__':
+    main()
+# okay decompiling __pycache__/main.cpython-37.pyc
